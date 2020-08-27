e78395c1b2d195b677bc8185f3841a53711f491c
doge <shadowphoenixcoc@gmail.com>
2020-08-26 21:21:51 -0400

    Update updateItemStatus.js

--- src/listeners/misc/updateItemStatus.js
+++ src/listeners/misc/updateItemStatus.js
@@ -13,14 +13,14 @@ module.exports = class updateCheckItemStateOnCard extends Listener {
             case "complete":
                 embed
                     .setTitle(`Checklist item marked complete!`)
-                    .setDescription(`**CARD:** ${event.data.card.name} — **[CARD LINK](https://trello.com/c/${event.data.card.shortLink})**\n\n**EVENT:** Checklist item under checklist \`${event.data.checklist.name}\` marked complete by **[${conf.realNames ? event.memberCreator.fullName : event.memberCreator.username}](https://trello.com/${event.memberCreator.username})**`)
+                    .setDescription(`**CARD:** ${event.data.card.name} — **[CARD LINK](https://trello.com/c/${event.data.card.shortLink})**\n\n**EVENT:** Checklist item under checklist \`${event.data.checklist.name}\` marked complete by **[${event.memberCreator.username}](https://trello.com/${event.memberCreator.username})**`)
                     .addField(`Checklist Item Name`, event.data.checkItem.name.length > 1024 ? `${event.data.checkItem.name.trim().slice(0, 1020)}...` : event.data.checkItem.name)
                 await this.client.post({ embeds: [embed] })
                 break;
             case "incomplete":
                 embed
                     .setTitle(`Checklist item marked incomplete!`)
-                    .setDescription(`**CARD:** ${event.data.card.name} — **[CARD LINK](https://trello.com/c/${event.data.card.shortLink})**\n\n**EVENT:** Checklist item under checklist \`${event.data.checklist.name}\` marked incomplete by **[${conf.realNames ? event.memberCreator.fullName : event.memberCreator.username}](https://trello.com/${event.memberCreator.username})**`)
+                    .setDescription(`**CARD:** ${event.data.card.name} — **[CARD LINK](https://trello.com/c/${event.data.card.shortLink})**\n\n**EVENT:** Checklist item under checklist \`${event.data.checklist.name}\` marked incomplete by **[${event.memberCreator.username}](https://trello.com/${event.memberCreator.username})**`)
                     .addField(`Checklist Item Name`, event.data.checkItem.name.length > 1024 ? `${event.data.checkItem.name.trim().slice(0, 1020)}...` : event.data.checkItem.name)
                 await this.client.post({ embeds: [embed] })
                 break;
